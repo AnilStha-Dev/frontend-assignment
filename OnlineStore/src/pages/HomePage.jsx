30d8c57f2f5f57636b8cf9eda7aefbcc73ab1aba
anil <anilaksshrest@gmail.com>
2023-07-21 08:11:44 +0545

    feat:useQuery is implemented to fetch api with axios

--- OnlineStore/src/pages/HomePage.jsx
+++ OnlineStore/src/pages/HomePage.jsx
@@ -1,27 +1,39 @@
 import React, { useState, useEffect } from 'react';
 import CardComp from '../Components/CardComp';
+import axios from "axios";
+import { useQuery } from 'react-query';
 
 const HomePage = () => {
-  const [products, setProducts] = useState([]);
-
   const getProducts = async()=>{
+    // try {
+    //     const response=await  fetch('https://fakestoreapi.com/products');
+    //     const data =await response.json();
+    //     setProducts(data);
+    // } catch (error) {
+    //     console.log(error);
+    // }
     try {
-        const response=await fetch('https://fakestoreapi.com/products');
-        const data =await response.json();
-        setProducts(data);
-    } catch (error) {
-        console.log(error);
-    }
+        const response = await axios.get('https://fakestoreapi.com/products');
+        return response.data;
+      } catch (error) {
+        console.error(error);
+      }
   }
-  useEffect(()=>{
-    getProducts()
-  },[]);
+  const { isLoading, isError, data:products, error } = useQuery('todos', getProducts);
+  if (isLoading) {
+    return <span>Loading...</span>
+  }
+
+  if (isError) {
+    return <span>Error: {error.message}</span>
+  }
   return (
     <>
-      {products.map((product) => {
-        return <CardComp key={product.id} product={product} />;
-      })}
-    </>
+    {products.map((product) => {
+      return <CardComp key={product.id} product={product} />;
+    })}
+  </>
+   
   );
 };
 
